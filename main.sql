41e1de666d59f873be797d4ece2cb9f359173c42
Sofia Covarrubias <77479635+sofia-covarrubias@users.noreply.github.com>
2022-05-23 00:41:46 +0000

    printed out entire list

--- main.sql
+++ main.sql
@@ -1,26 +1,30 @@
 .header on
-.mode column 
+.mode column
 
 CREATE TABLE internships
 (
   title TEXT,
   company TEXT,
   description TEXT,
-  location TEXT,
   salaryPerHour DOUBLE,
+  startDate DATE,
   deadline DATE
 );
 
 INSERT INTO internships values 
-("Visual Analytics Intern", "Pacific Northwest National Laboratory", "Address complex data and analytic challenges.", "Virtual, United States", 0.00, 05/26/2022),
-("Coding Intructor", "Code Ninjas", "Teaching kids to code.", "Ontario, CA / Virtual on Zoom", 14.00, 05/31/2022),
-("Web Development Intern", "Anaheim Automation", "PHP, JavaScript, MySQL, HTML, CSS", "Includes Riverside, CA", 14.00, 05/31/2022),
-("STEM Coding + Raspberry Pi Instructors", "Delaware STEAM Academy", "Art-time Scratch coding, Python, Unity and Machine Learning coaches", "Remote", 15.00, 05/30/2022),
-("Mobile Developer Intern", "M2 Interactive, LLC", "Qualified Mobile developer to join our Engineering team.", "Remote", 150.00, 05/30/2022),
-("App Development Startup Intern", "Hideouts", "Web Developments", "Remote", 0.00, 05/30/2022),
-("Deep Learning Classification of Kepler/TESS Transit Signals", "NASA", "Understanding of how common earth-like planets and the chances of alien life are.", "Virtual", 0.00, 05/30/2022),
-("AI & Machine Learning Techniques for Airspace Operations", "NASA", "AI/ML (e.g., Natural Language Processing) to digitize and analyze heritage air traffic", "Virtual", 0.00, 05/30/2022),
-("Data Visualization System for SJC Airport Ground Traffic", "NASA", "Data visualization system of arrival / departure air traffic data.", "Virtual", 0.00, 05/30/2022),
-("Develop Aviation-Specific Machine Learning Model (using BERT)", "NASA", "Initiate and develop aviation domain-specific language model.", "Virtual", 0.00, 05/30/2022),
-("Advanced Air Mobility National Campaign", "NASA", "building up an ecosystem to enable urban air mobility through a series of flight demonstrations and simulations", "Virtual", 0.00, 05/30/2022),
-("CA OSTEM Internship", "NASA", "marketing, business, graphic design", "Virtual", 0.00, 05/30/2022);
+("Visual Analytics Intern", "Pacific Northwest National Laboratory", "Address complex data and analytic challenges.", 0.00, "2022-05-01", "2022-05-26"),
+("Coding Intructor", "Code Ninjas", "Teaching kids to code.", 14.00, "2022-05-01", "2022-05-26"),
+("Web Development Intern", "Anaheim Automation", "PHP, JavaScript, MySQL, HTML, CSS", 14.00, "2022-05-01", "2022-05-30"),
+("STEM Coding + Raspberry Pi Instructors", "Delaware STEAM Academy", "Scratch, Python, Unity and Machine Learning coaches", 15.00, "2022-05-01", "2022-05-26"),
+("Mobile Developer Intern", "M2 Interactive, LLC", "Qualified Mobile developer to join our Engineering team.", 150.00, "2022-05-01", "2022-05-31"),
+("App Development Startup Intern", "Hideouts", "Web Developments", 0.00, "2022-05-01", "2022-05-30"),
+("Kepler/TESS Transit Signals", "NASA", "Understanding of how common earth-like planets and the chances of alien life are.", 0.00, "2022-05-01", "2022-05-31"),
+("AI/Machine Learning for Airspace", "NASA", "AI/ML (e.g., Natural Language Processing) to digitize and analyze heritage air traffic", 0.00, "2022-05-01", "2022-05-31"),
+("Data Visualization System", "NASA", "Data visualization system of arrival / departure air traffic data.", 0.00, "2022-05-01", "2022-05-31"),
+("Develop Aviation-Specific ML Model", "NASA", "Initiate and develop aviation domain-specific language model.", 0.00, "2022-05-01", "2022-05-31"),
+("Advanced Air Mobility National Campaign", "NASA", "building up an ecosystem to enable urban air mobility", 0.00, "2022-05-01", "2022-05-31"),
+("CA OSTEM Internship", "NASA", "marketing, business, graphic design", 0.00, "2022-05-01", "2022-05-31");
+
+--- OUTPUT ENTIRE LIST
+.print 'OUTPUT ENTIRE LIST'
+SELECT * FROM internships;
